0e31ecd640082b44e2f54ca4fdba1e9a5585f0b2
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-12 14:53:52 -0600

    Basic Dummy DropDown with sweet transition:

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 // import NavBar from "@/app/components/NavBar";
-import Table from "@/app/components/Table";
+// import Table from "@/app/components/Table";
+import DropDown from "@/app/components/DropDown";
 
 export default function Home() {
     return (
-        <>
-            <Table />
-        </>
+        <div className="flex justify-center">
+            <DropDown />
+        </div>
     );
 }
