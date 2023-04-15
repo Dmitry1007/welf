ba8552b31c45f577bdcf602df0273736677c8e34
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-14 18:36:29 -0600

    Add stock to table

--- src/app/components/Table.js
+++ src/app/components/Table.js
@@ -22,14 +22,20 @@ export default function Table({ data }) {
 
   const handleAddStock = (event) => {
     event.preventDefault();
-    console.log(
-      "symbol",
-      symbol,
-      "quantity",
-      quantity,
-      "pricePerShare",
-      pricePerShare
-    );
+    setStocks([
+      ...stocks,
+      {
+        symbol,
+        quantity,
+        pricePerShare,
+        totalCost: "COMPUTE",
+        currentPrice: "FETCH",
+        marketValue: "COMPUTE",
+      },
+    ]);
+    setSymbol("");
+    setQuantity("0");
+    setPricePerShare("0");
   };
 
   return (
@@ -129,7 +135,6 @@ export default function Table({ data }) {
                       </td>
                     </tr>
                   ))}
-
                   <tr>
                     <td className="whitespace-nowrap py-2 pl-4 pr-3 text-sm text-gray-500 sm:pl-0">
                       <input
@@ -157,7 +162,7 @@ export default function Table({ data }) {
                         onChange={(e) => setPricePerShare(e.target.value)}
                         value={pricePerShare}
                         type="decimal"
-                        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                         placeholder="Price"
                       />
                     </td>
